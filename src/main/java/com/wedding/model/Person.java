ff698e48646287eb6488664dab2338feedf544bb
IT21327926 <it21327926@my.sliit.lk>
2026-05-08 16:01:26 +0530

    Fix: Map Person fields to database with @MappedSuperclass

--- src/main/java/com/wedding/model/Person.java
+++ src/main/java/com/wedding/model/Person.java
@@ -1,5 +1,8 @@
 package com.wedding.model;
 
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public abstract class Person {
     private String name;
     private String email;
@@ -13,7 +16,6 @@ public abstract class Person {
         this.phone = phone;
     }
 
-    // Getters and Setters
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
@@ -23,6 +25,5 @@ public abstract class Person {
     public String getPhone() { return phone; }
     public void setPhone(String phone) { this.phone = phone; }
 
-    // Polymorphism
     public abstract String getRole();
 }
